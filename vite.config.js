21241098fc5a47520b02b27321b16b71af5d3742
Neeraj <neerajsaraswat111@gmail.com>
2024-09-28 22:15:13 +0530

    made changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 export default {
-  root: "src/",
-  publicDir: "../static/",
-  base: "./",
+  root: "src/", // Root of your project
+  publicDir: "../static/", // Static assets directory
+  base: "./", // Relative paths for assets
+
+  build: {
+    outDir: "../dist", // Output directory for build
+  },
 };
